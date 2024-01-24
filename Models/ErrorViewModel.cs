c4cb59c7d8b22a3196e40f102de0515a09250d22
samanehesh <145700227+samanehesh@users.noreply.github.com>
2024-01-24 13:51:15 -0800

    new commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SaltPepper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
